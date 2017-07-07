8af59cc3df9f11f59d6b1344540d7dbd6c2e250f
David Skeppstedt <david.skeppstedt@gmail.com>
2017-07-07 20:05:31 +0200

    Update app to connect to db

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"lista/api/server"
+	"lista/api/db"
 	"log"
 )
 
@@ -9,6 +10,8 @@ var version = "Version 1"
 var name = "Lista-API"
 
 func main() {
+	m := db.NewMongoDb("db")
+	m.Connect()
 	log.Println("Starting", name, version)
 	server.Start(":1337")
 }
